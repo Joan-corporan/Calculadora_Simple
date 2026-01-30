c09fa83eb9192bbe1cf8d3ebf7358a6490e725c7
Joan Corporan <joan.corporan.h@gmail.com>
2026-01-30 15:02:15 -0300

    fix: keep modulo_joan only inside addons

---
+++ Regreso/Odoo_Practica/modulo_Joan/addons/modulo_joan/_manifest_.py
@@ -0,0 +1,13 @@
+{
+    "name": "Modulo Joan",
+    "version": "1.0",
+    "summary": "Modulo con menu visible",
+    "category": "Custom",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
